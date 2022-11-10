99c52955d03ea6adf4728bf0e53057b5ab3134bd
Tutor <tutorbuki@gmail.com>
2022-11-10 14:59:39 +0200

    contact form styles

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -9,7 +9,7 @@ export default function ContactForm() {
     } = useForm();
     
     const onSubmit = (data) => console.log(data);
-    // console.log(errors);
+    console.log(errors);
 
 
   const btn_style = `flex text-white font-medium bg-gray-700 border-0
@@ -18,56 +18,83 @@ export default function ContactForm() {
       dark:hover:text-white
   `
 
+  const input_holder_cls = `py-2 w-full`
+
+  const input_cls = `dark:bg-transparent w-full bg-gray-100 bg-opacity-50
+      rounded border border-gray-300 focus:border-violet-400
+      focus:bg-white focus:ring-2 focus:ring-violet-200 text-base outline-none
+      text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out
+  `
+
   return (
-      <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="flex flex-wrap -m-2 text-md">
-            <div className="p-2 w-full">
-              <div className="relative">
-                <label htmlFor="name" className="leading-7 text-gray-600">
+      <form onSubmit = { handleSubmit(onSubmit) }>
+          <div className = "flex flex-wrap text-md">
+            <div className = {input_holder_cls} >
+              <div className ="relative">
+                
+                <label
+                  htmlFor="name"
+                  className="leading-7 text-gray-600"
+                >
                   Name
                 </label>
+                
                 <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  className="dark:bg-transparent w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-violet-400 focus:bg-white focus:ring-2 focus:ring-violet-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  type = "text"
+                  id = "name"
+                  name = "name"
+                  className = { input_cls }
                   {...register("name", { required: true })}
                 />
+              
               </div>
             </div>
-            <div className="p-2 w-full">
+            
+            <div className = { input_holder_cls } >
               <div className="relative">
-                <label htmlFor="email" className="leading-7 text-gray-600">
+                
+                <label
+                  htmlFor = "email"
+                  className = "leading-7 text-gray-600"
+                >
                   Email
-                </label>
+                </label>  
+                
                 <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  className="dark:bg-transparent w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-violet-400 focus:bg-white focus:ring-2 focus:ring-violet-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  type = "email"
+                  id = "email"
+                  name = "email"
+                  className = { input_cls }
                   {...register("email", { required: true })}
                 />
+                
               </div>
             </div>
 
-            <div className="p-2 w-full">
+            <div className = { input_holder_cls } >
               <div className="relative">
-                <label htmlFor="message" className="leading-7 text-gray-600">
+                
+                <label
+                  htmlFor = "message"
+                  className = "leading-7 text-gray-600"
+                >
                   Message
                 </label>
+
                 <textarea
-                  id="message"
-                  name="message"
-                  className="dark:bg-transparent w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-violet-400 focus:bg-white focus:ring-2 focus:ring-violet-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                  id = "message"
+                  name = "message"
+                  className = { input_cls }
                   {...register("message")}
                 ></textarea>
+              
               </div>
             </div>
 
-            <div className="p-2 w-full">
-              <button className = {btn_style} >
-                Send Message
-              </button>
+            <div className = { input_holder_cls } >
+                <button className = { btn_style } >
+                  Send Message
+                </button>
             </div>
           </div>
         </form>
--- pages/contact.js
+++ pages/contact.js
@@ -1,27 +1,36 @@
 import Layout from "../components/Layout";
 import Link from 'next/link';
-// import ContactForm from "../components/ContactForm";
+import ContactForm from "../components/ContactForm";
 
 export default function Contact() {
-  const email_style = `font-bold underline hover:text-violet-400`
+  const email_cls = `font-bold underline hover:text-violet-400`
+  const heading_cls = `font-sans text-4xl font-extrabold
+      leading-none tracking-tight my-3 dark:text-gray-300
+  `
+  const address_cls = `not-italic text-lg dark:text-gray-300`
+  const section_cls = `text-gray-600 body-font relative`
+  const holder_cls = `lg:w-1/3 md:w-96 mx-auto`
+  const adr_holder_cls = `flex text-xl flex-col w-full my-12`
 
   return (
     <Layout>
       <div className="max-w-screen px-4 py-12 mx-auto">
       
-        <section className="text-gray-600 body-font relative">                
-            <div className="lg:w-1/3 md:w-2/3 mx-auto">
-                <div className="font-sans text-4xl font-extrabold leading-none tracking-tight my-3 dark:text-gray-300">
+        <section className = { section_cls } >                
+            <div className = { holder_cls } >
+                <div className = { heading_cls } >
                     Contact
                 </div>
-                <div className="flex text-xl flex-col w-full my-12">
-                <address className = "not-italic text-lg dark:text-gray-300">
-                    Type me an email on <Link href="mailto:myko@grymyk.com">
-                        <a className = {email_style} >myko@grymyk.com</a>
+
+                <div className = { adr_holder_cls } >
+                  <address className = { address_cls } >
+                    Get in touch or type me an email on <Link href="mailto:myko@grymyk.com">
+                        <a className = { email_cls } >myko@grymyk.com</a>
                     </Link>
                   </address>
                 </div>
-                {/* <ContactForm /> */}
+
+                <ContactForm />
             </div>
         </section>
 
