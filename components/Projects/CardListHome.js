127a9a412d7f34aff4574cb85239365d60d12d3d
Mykola Grytsai <grymyk@gmail.com>
2023-07-17 15:41:03 +0300

    show 4 cards

--- components/Projects/CardListHome.js
+++ components/Projects/CardListHome.js
@@ -1,11 +1,11 @@
 import Card from './Card.js'
 
 export default function WorkCardListHome({data}) {
-    const numberCards = 3;
+    const numberCards = 4;
     
     return (
         <>
-            <div className="grid md:grid-cols-3 gap-8 lg:mt-8 pb-40 text-center">
+            <div className="grid md:grid-cols-2 gap-8 lg:mt-8 pb-40 text-center">
                 { data && 
                     data
                     .slice(0, numberCards)
